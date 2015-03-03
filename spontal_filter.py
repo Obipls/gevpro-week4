bc93f55d38d8245908a1f1d28aa9fcb479a6c3d6
Olivier Louwaars <o_louwaars@hotmail.com>
2015-03-03 13:33:31 +0100

    Commentaar erbij

--- spontal_filter.py
+++ spontal_filter.py
@@ -7,17 +7,22 @@ import sys
 import xml.etree.ElementTree as ET
 
 def main(argv):
+	# Check for valid program call
 	if len(argv)<3:
 		print("Usage = spontal_filter.py spontal.xml spontal_filtered.xml")
 		exit(-1)
 	
+	# Open XML-file and variablelize it's children
 	tree=ET.parse(argv[1])
 	root=tree.getroot()
 	x=0
 	for child in root:
+		# Check if the start and end pitch are within the range from bottom to top
 		if not float(root[x][5].text)<=float(root[x][12].text)<=float(root[x][7].text) or not float(root[x][5].text)<=float(root[x][13].text)<=float(root[x][7].text):
+			# Remove them if they are out of range
 			root.remove(child)
 		x+=1
+	# Write all the correct values to a new document
 	tree.write(argv[2])
 
 if __name__=="__main__":
