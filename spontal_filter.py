7ecf5a48d96548148e91a78f0a2361ec06bf629d
Olivier Louwaars <o_louwaars@hotmail.com>
2015-03-03 12:26:40 +0100

    hoi

---
+++ spontal_filter.py
@@ -0,0 +1,17 @@
+# #!/usr/bin/env python
+# spontal_filter.py
+# Olivier Louwaars s2814714
+# 3-3-2015
+
+import sys
+
+def main(argv):
+	if len(argv)<3:  # check if there are arguments
+		print("Usage = spontal_filter.py spontal.xml spontal_filtered.xml")
+		exit(-1)
+
+
+
+
+if __name__=="__main__":
+	main(sys.argv)
