454e4da6060411433f1824d767627bae1a2a61e2
Olivier Louwaars <o_louwaars@hotmail.com>
2015-03-03 16:31:33 +0100

    Opdracht twee ook werkend, met namedtuple en list comprehension

---
+++ blood-die.py
@@ -0,0 +1,28 @@
+# #!/usr/bin/env python
+# blood-die.py
+# Olivier Louwaars s2814714
+# 3-3-2015
+
+import sys
+import json
+from collections import namedtuple
+
+def main(argv):
+	# Check for valid program call
+	if len(argv)<2:
+		print("Usage = blood-die.py blood-die.json")
+		exit(-1)
+
+	langList=json.load(open(argv[1]))
+	langWithWord=namedtuple('langWithWord','language, langClass,bloodWords,dieWords')
+	simiLangList=[langWithWord(attr[0],attr[1],attr[2].strip().split(', '),attr[3].strip().split(', ')) for attr in langList]
+	
+	resList=[]
+	for item in simiLangList:
+		for text in item.bloodWords:
+			if text in item.dieWords:
+				resList.append([item.language,item.langClass])
+	print(resList)
+
+if __name__=="__main__":
+	main(sys.argv)
