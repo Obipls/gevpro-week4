75a827905d96c82826cd38655df8762d7acb19ba
Olivier Louwaars <o_louwaars@hotmail.com>
2015-03-03 13:27:24 +0100

    Programma werkend, geen tussentijdse commits

--- spontal_filter.py
+++ spontal_filter.py
@@ -4,14 +4,21 @@
 # 3-3-2015
 
 import sys
+import xml.etree.ElementTree as ET
 
 def main(argv):
-	if len(argv)<3:  # check if there are arguments
+	if len(argv)<3:
 		print("Usage = spontal_filter.py spontal.xml spontal_filtered.xml")
 		exit(-1)
-
-
-
+	
+	tree=ET.parse(argv[1])
+	root=tree.getroot()
+	x=0
+	for child in root:
+		if not float(root[x][5].text)<=float(root[x][12].text)<=float(root[x][7].text) or not float(root[x][5].text)<=float(root[x][13].text)<=float(root[x][7].text):
+			root.remove(child)
+		x+=1
+	tree.write(argv[2])
 
 if __name__=="__main__":
 	main(sys.argv)
